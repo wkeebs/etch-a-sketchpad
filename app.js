6abcdc4b027daa993a2d623965ca96cae2b95d23
Keebs MacBook Pro <will.a.keeble@gmail.com>
2023-07-29 13:09:01 +1000

    Implemented resizing of grid with the slider. Updated colour values in CSS.

--- app.js
+++ app.js
@@ -1,26 +1,32 @@
+// Default Values
 const DEFAULT_SIZE = 4;
 const DEFAULT_COLOUR = "black";
 
+// Variable Values
 let currentSize = DEFAULT_SIZE;
 let currentColour = DEFAULT_COLOUR;
 
+// UI Elements
 const container = document.querySelector("#grid-container");
 const slider = document.querySelector(".slider");
 const sliderDisplay = document.querySelector("#slider-value");
 
+// Functions
 function updateSliderValue(value) {
     sliderDisplay.innerHTML = `${value} x ${value}`;
 }
 
+function changeGridColumns(columns) {
+    container.style.gridTemplateColumns = `repeat(${columns}, 1fr)`;
+}
+
 function initialiseGrid(size) {
     for (i=0; i < size * size; i++) {
+        changeGridColumns(size);
         const newDiv = document.createElement("div");
 
         newDiv.addEventListener("mouseover", () => {
             newDiv.style.backgroundColor = "black";
-            setTimeout(() => {
-                newDiv.style.backgroundColor = "white";
-            }, 500);
         });
 
         newDiv.classList.add("grid-div");
@@ -28,13 +34,23 @@ function initialiseGrid(size) {
     }
 }
 
-function initialiseElements() {
+function initialisePage() {
     initialiseGrid(DEFAULT_SIZE);
     updateSliderValue(DEFAULT_SIZE);
     slider.oninput = () => {
         currentSize = slider.value;
         updateSliderValue(currentSize);
+        changeSize();
     }
 }
 
-initialiseElements();
+function changeSize() {
+    container.innerHTML = "";
+    initialiseGrid(currentSize);
+}
+
+// Initialise Page
+window.onload = () => {
+    initialisePage();
+}
+
