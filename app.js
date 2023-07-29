1e4197ae5997bb0a63727e89c940b26404e74dd3
Keebs MacBook Pro <will.a.keeble@gmail.com>
2023-07-29 12:45:07 +1000

    Clean up JS file, moving into functions. Finish slider.

--- app.js
+++ app.js
@@ -1,10 +1,40 @@
-const container = document.querySelector("#grid-container");
+const DEFAULT_SIZE = 4;
+const DEFAULT_COLOUR = "black";
 
-for (i=0; i < 16; i++) {
-    const newDiv = document.createElement("div");
-    newDiv.addEventListener("mouseover", () => {
-        newDiv.style.backgroundColor = "black";
-    });
-    newDiv.classList.add("grid-div");
-    container.appendChild(newDiv);
+let currentSize = DEFAULT_SIZE;
+let currentColour = DEFAULT_COLOUR;
+
+const container = document.querySelector("#grid-container");
+const slider = document.querySelector(".slider");
+const sliderDisplay = document.querySelector("#slider-value");
+
+function updateSliderValue(value) {
+    sliderDisplay.innerHTML = `${value} x ${value}`;
 }
+
+function initialiseGrid(size) {
+    for (i=0; i < size * size; i++) {
+        const newDiv = document.createElement("div");
+
+        newDiv.addEventListener("mouseover", () => {
+            newDiv.style.backgroundColor = "black";
+            setTimeout(() => {
+                newDiv.style.backgroundColor = "white";
+            }, 500);
+        });
+
+        newDiv.classList.add("grid-div");
+        container.appendChild(newDiv);
+    }
+}
+
+function initialiseElements() {
+    initialiseGrid(DEFAULT_SIZE);
+    updateSliderValue(DEFAULT_SIZE);
+    slider.oninput = () => {
+        currentSize = slider.value;
+        updateSliderValue(currentSize);
+    }
+}
+
+initialiseElements();
