92baa5aec587e27c516bc8e4ff3c9355935ab047
Will Keeble <will.a.keeble@gmail.com>
2023-08-25 11:28:37 +1000

    Added header and footer

--- app.js
+++ app.js
@@ -13,44 +13,43 @@ const sliderDisplay = document.querySelector("#slider-value");
 
 // Functions
 function updateSliderValue(value) {
-    sliderDisplay.innerHTML = `${value} x ${value}`;
+  sliderDisplay.innerHTML = `${value} x ${value}`;
 }
 
 function changeGridColumns(columns) {
-    container.style.gridTemplateColumns = `repeat(${columns}, 1fr)`;
+  container.style.gridTemplateColumns = `repeat(${columns}, 1fr)`;
 }
 
 function initialiseGrid(size) {
-    for (i=0; i < size * size; i++) {
-        changeGridColumns(size);
-        const newDiv = document.createElement("div");
+  for (i = 0; i < size * size; i++) {
+    changeGridColumns(size);
+    const newDiv = document.createElement("div");
 
-        newDiv.addEventListener("mouseover", () => {
-            newDiv.style.backgroundColor = "black";
-        });
+    newDiv.addEventListener("mouseover", () => {
+      newDiv.style.backgroundColor = "black";
+    });
 
-        newDiv.classList.add("grid-div");
-        container.appendChild(newDiv);
-    }
+    newDiv.classList.add("grid-div");
+    container.appendChild(newDiv);
+  }
 }
 
 function initialisePage() {
-    initialiseGrid(DEFAULT_SIZE);
-    updateSliderValue(DEFAULT_SIZE);
-    slider.oninput = () => {
-        currentSize = slider.value;
-        updateSliderValue(currentSize);
-        changeSize();
-    }
+  initialiseGrid(DEFAULT_SIZE);
+  updateSliderValue(DEFAULT_SIZE);
+  slider.oninput = () => {
+    currentSize = slider.value;
+    updateSliderValue(currentSize);
+    changeSize();
+  };
 }
 
 function changeSize() {
-    container.innerHTML = "";
-    initialiseGrid(currentSize);
+  container.innerHTML = "";
+  initialiseGrid(currentSize);
 }
 
 // Initialise Page
 window.onload = () => {
-    initialisePage();
-}
-
+  initialisePage();
+};
